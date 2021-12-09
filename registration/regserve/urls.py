a38bc0a87c10fe75bfe3f88f1d1aa4b395bc6402
Evan <ehenderson@zagmail.gonzaga.edu>
2021-12-09 15:53:28 -0800

    django form and hml template

--- registration/regserve/urls.py
+++ registration/regserve/urls.py
@@ -5,6 +5,7 @@ from . import views
 application = 'regserve'
 
 urlpatterns = [
+    path('/students/', views.StudentListForm.as_view(), name="students"),
     path('data/students/', views.StudentListCreate.as_view()),
     path('', views.index, name='index'),
     ]
--- registration/regserve/views.py
+++ registration/regserve/views.py
@@ -4,6 +4,7 @@ from .models import *
 from rest_framework import serializers
 from .serializers import *
 from rest_framework import generics
+from django.views.generic import ListView, CreateView
 
 # Create your views here.
 def index(request):
@@ -13,4 +14,5 @@ class StudentListCreate(generics.ListCreateAPIView):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
 
-
+class StudentListForm(ListView):
+    model = Student
