c1bd418727f13ebf2607147c2c1e2c8690dbde47
Evan <ehenderson@zagmail.gonzaga.edu>
2021-12-09 16:23:08 -0800

    application to app_name in urls.py

--- registration/regserve/urls.py
+++ registration/regserve/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from . import views
 
-application = 'regserve'
+app_name = 'regserve'
 
 urlpatterns = [
     path('/students/', views.StudentListForm.as_view(), name="students"),
