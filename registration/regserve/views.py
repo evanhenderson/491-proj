86822a51dea277e694203c862fdcffdb191dde06
Evan <ehenderson@zagmail.gonzaga.edu>
2021-12-12 19:35:59 -0800

    added rest of CRUD (delete and edit)

--- registration/regserve/views.py
+++ registration/regserve/views.py
@@ -4,11 +4,23 @@ from .models import *
 from rest_framework import serializers
 from .serializers import *
 from rest_framework import generics
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, DeleteView, UpdateView
 
 # Create your views here.
 def index(request):
     return HttpResponse("Hello from Django backend")
+
+class StudentDelete(DeleteView):
+    model = Student
+    pk_url_kwarg = 'pk'
+    template_name = 'regserve/student_delete_form.html'
+    success_url = 'regserve/students'
+
+class StudentEdit(UpdateView, ListView):
+    model = Student
+    pk_url_kwarg = 'pk'
+    template_name = 'regserve/student_edit_form.html'
+    success_url = 'regserve/students'
 
 class StudentListCreate(generics.ListCreateAPIView):
     queryset = Student.objects.all()
